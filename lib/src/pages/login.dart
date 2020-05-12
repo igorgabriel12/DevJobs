f9f801ca45f90d78c1d4a50bb4d5a6afca5ba4f7
unknown <igor_gabryel@hotmail.com>
2020-05-12 10:32:13 -0300

    Login screen layout finished

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -20,8 +20,8 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  TextEditingController txtPeso = TextEditingController();
-  TextEditingController txtAltura = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -38,55 +38,110 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.symmetric(vertical: 90.0),
+              padding: EdgeInsets.only(top: 90.0, bottom: 30.0),
               child: Container(
-                  height: 150,
+                  height: 150.0,
                   child: Image.asset(
                     "assets/images/github_logo.png",
                     // scale: 1.8,
                   )),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black,
-                        blurRadius: 5.0,
-                      ),
-                    ],
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                height: 200.0,
-                child: Column(
+                padding: EdgeInsets.only(left: 40.0, bottom: 10.0),
+                child: Row(
                   children: <Widget>[
-                    Container(
-                      child: Row(
+                    Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ],
+                )),
+            SizedBox(
+                height: 200,
+                child:
+                    Stack(alignment: Alignment.bottomCenter, children: <Widget>[
+                  Container(
+                    height: 235,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              // blurRadius: 5.0,
+                            ),
+                          ],
+                          color: Colors.white,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0))),
+                      height: 180.0,
+                      margin: EdgeInsets.only(bottom: 24.0),
+                      child: Column(
                         children: <Widget>[
-                          Icon(
-                            Icons.mail,
-                            size: 30,
-                            color: Colors.red[700],
-                          ),
-                          // Container(
-                          //   padding: EdgeInsets.symmetric(vertical: 10),
-                          //   child: TextField(
-                          //     decoration: InputDecoration(
-                          //         border: InputBorder.none,
-                          //         hintText: 'Enter a search term'),
-                          //   ),
-                          // )
+                          _entradaDados('E-mail', email, Icons.mail),
+                          _entradaDados('Password', password, Icons.lock),
                         ],
                       ),
-                    )
-                  ],
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("Container clicked");
+                      //  if (_formKey.currentState.validate()) {
+                      //   setState(() {
+                      //     double peso = double.parse(txtPeso.text);
+                      //     double altura = double.parse(txtAltura.text);
+                      //     double imc = peso / pow(altura, 2);
+                      //     _resultado = "Resultado\nIMC= ${imc}";
+                      //   });
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 1.0,
+                            ),
+                          ],
+                          color: "#4A65B0".toColor(),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(25.0))),
+                      height: 45.0,
+                      width: 200.0,
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  )
+                ])),
+            InkWell(
+              onTap: () {
+                print("Container clicked");
+                //  if (_formKey.currentState.validate()) {
+                //   setState(() {
+                //     double peso = double.parse(txtPeso.text);
+                //     double altura = double.parse(txtAltura.text);
+                //     double imc = peso / pow(altura, 2);
+                //     _resultado = "Resultado\nIMC= ${imc}";
+                //   });
+              },
+              child: Padding(
+                padding: EdgeInsets.only(top: 70.0),
+                child: Text(
+                  "Create account?",
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 100.0),
-              child: Text("Create account?"),
             ),
           ],
         ),
@@ -95,15 +150,31 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   //Métodos
-  // _entradaDados(rotulo, controle) {
-  //   return Container(
-  //     padding: EdgeInsets.symmetric(vertical: 10),
-  //     child: TextField(
-  //       decoration: InputDecoration(
-  //           border: InputBorder.none, hintText: 'Enter a search term'),
-  //     ),
-  //   );
-  // } //
+  _entradaDados(label, controle, icon) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: Container(
+          padding:
+              EdgeInsets.only(top: label == 'E-mail' ? 15.0 : 0, left: 20.0),
+          child: TextFormField(
+            controller: controle,
+            validator: (value) {
+              return (value.isEmpty) ? "Informe o valor" : null;
+            },
+            decoration: InputDecoration(
+                icon: Icon(
+                  icon,
+                  size: 22,
+                  color: "#4A65B0".toColor(),
+                ),
+                border: InputBorder.none,
+                hintText: label),
+          ),
+        ))
+      ],
+    );
+  }
 }
 
 //    TextField(
